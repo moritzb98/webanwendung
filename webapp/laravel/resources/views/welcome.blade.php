fa85557d0edf430e8a44d3782d90196c0e9811e0
Jana <jana.ballweg@gmail.com>
2020-02-05 09:59:39 +0100

    Edit startpage

--- webapp/laravel/resources/views/welcome.blade.php
+++ webapp/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Hallo Moritz
                 </div>
 
                 <div class="links">
