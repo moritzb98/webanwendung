0c86ad23df3ab4e28dd88ecca587e1eede2e8df0
Fabulous <Fabi>
2020-02-05 10:15:16 +0100

    Auto stash before merge of "master" and "origin/master"

--- webapp/laravel/resources/views/welcome.blade.php
+++ webapp/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
+<<<<<<< Updated upstream
                    Veranstaltungsmanager
+=======
+                    Laravel
+>>>>>>> Stashed changes
                 </div>
 
                 <div class="links">
