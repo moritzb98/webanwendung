c3eab97e45a9c8ace46f2410f7d764bf053e39e3
Theresa <theresabrenner@gmx.net>
2020-02-05 10:05:16 +0100

    Name geändert auf der Startseite

--- webapp/laravel/resources/views/welcome.blade.php
+++ webapp/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hallo Moritz
+                   Veranstaltungsmanager
                 </div>
 
                 <div class="links">
