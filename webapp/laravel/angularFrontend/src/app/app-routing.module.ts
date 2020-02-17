3e5585f02c3cd339558372722349584df917052a
Theresa <theresabrenner@gmx.net>
2020-02-17 10:00:28 +0100

    html auf der Startseite

--- webapp/laravel/angularFrontend/src/app/app-routing.module.ts
+++ webapp/laravel/angularFrontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { StartseiteComponent } from './startseite/startseite.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,6 +8,11 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+
+  {
+    path: 'start',
+    component: StartseiteComponent,
   }
 ];
 
