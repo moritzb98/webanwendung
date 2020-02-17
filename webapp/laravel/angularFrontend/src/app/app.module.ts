ded0a3cbe3a07ff1605fbf1f5cfabeef8fecfcb9
Theresa <theresabrenner@gmx.net>
2020-02-17 09:44:19 +0100

    Startseitenkomponent erstellt

--- webapp/laravel/angularFrontend/src/app/app.module.ts
+++ webapp/laravel/angularFrontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HomeService } from './home.service';
+import { StartseiteComponent } from './startseite/startseite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    StartseiteComponent,
   ],
   imports: [
     BrowserModule,
