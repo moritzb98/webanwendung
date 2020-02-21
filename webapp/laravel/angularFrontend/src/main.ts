4692ffe3c5f996e7babefd47cc40f4459a4248f4
Theresa <theresabrenner@gmx.net>
2020-02-21 22:43:34 +0100

    edit login site - button + Logo src

--- webapp/laravel/angularFrontend/src/main.ts
+++ webapp/laravel/angularFrontend/src/main.ts
@@ -5,9 +5,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {MDCRipple} from '@material/ripple';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
