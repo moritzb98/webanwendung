2cc37f71d9fe4fa1dd32976fee2ad99343c0d488
Moritz <moritz.banhardt@web.de>
2020-03-19 20:16:40 +0100

    toggle in Kontaktliste

--- webapp/laravel/angularFrontend/src/app/kontaktliste/kontaktliste.component.ts
+++ webapp/laravel/angularFrontend/src/app/kontaktliste/kontaktliste.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KontaktlisteComponent implements OnInit {
   toggleCatering = true;
+  toggleDrinks = true;
+  toggleEntertainment = true;
+  toggleLocation = true;
   constructor() { }
 
   ngOnInit() {
