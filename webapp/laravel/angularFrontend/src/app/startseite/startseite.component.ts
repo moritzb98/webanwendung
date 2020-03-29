eebb386923cdaeb9b90e0ae69cc3f10a67c28aec
Moritz <moritz.banhardt@web.de>
2020-03-29 12:35:06 +0200

    backend home

--- webapp/laravel/angularFrontend/src/app/startseite/startseite.component.ts
+++ webapp/laravel/angularFrontend/src/app/startseite/startseite.component.ts
@@ -1,3 +1,4 @@
+import { EventService } from './../Services/event.service';
 import { UserService } from './../Services/user.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,18 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartseiteComponent implements OnInit {
   public users: any = [];
+  public events: any = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private eventService: EventService) { }
 
   ngOnInit() {
     this.getUsers();
+    this.getEvents();
   }
 
   getUsers() {
     this.users = this.userService.getUsers().subscribe(
       data => {
         this.users = data.users;
-        console.log(this.users);
       },
       err => {
         console.log(err);
@@ -29,5 +31,16 @@ export class StartseiteComponent implements OnInit {
 
   getUser(id) {
     localStorage.setItem('userID', id);
+  }
+
+  getEvents() {
+    const eventsData = this.eventService.getEvents().subscribe(
+      data => {
+        this.events = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
