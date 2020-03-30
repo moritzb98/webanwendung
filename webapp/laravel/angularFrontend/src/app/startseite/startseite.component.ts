b2198fedcc64b613fa67f082e38bac33eb140ed4
Moritz <moritz.banhardt@web.de>
2020-03-30 16:14:08 +0200

    todos on home

--- webapp/laravel/angularFrontend/src/app/startseite/startseite.component.ts
+++ webapp/laravel/angularFrontend/src/app/startseite/startseite.component.ts
@@ -1,6 +1,7 @@
 import { EventService } from './../Services/event.service';
 import { UserService } from './../Services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../Services/todo.service';
 
 @Component({
   selector: 'app-startseite',
@@ -10,12 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class StartseiteComponent implements OnInit {
   public users: any = [];
   public events: any = [];
+  todo = null;
+  todos: any = [];
+  todosAdded: any = [];
 
-  constructor(private userService: UserService, private eventService: EventService) { }
+  constructor(private userService: UserService, private eventService: EventService, private todoService: TodoService) { }
 
   ngOnInit() {
     this.getUsers();
     this.getEvents();
+    this.getTodos();
   }
 
   getUsers() {
@@ -37,6 +42,29 @@ export class StartseiteComponent implements OnInit {
     const eventsData = this.eventService.getEvents().subscribe(
       data => {
         this.events = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  check(id, index) {
+    this.todos[index].checked = true;
+    const checkTodo = this.todoService.delete(id).subscribe(
+      data => {
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  getTodos() {
+    const todoData = this.todoService.getTodos().subscribe(
+      data => {
+        this.todos = data;
       },
       err => {
         console.log(err);
